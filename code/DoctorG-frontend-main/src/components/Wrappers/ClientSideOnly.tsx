0224bf1ce6d0ad01a8debd1f39055d4838bb4b9a
AbhiTom2000 <abhitom2000@gmail.com>
2024-09-01 03:37:04 +0530

    removed src

--- code/DoctorG-frontend-main/src/components/Wrappers/ClientSideOnly.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-import { ReactNode } from 'react';
-
-const ClientOnly = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default dynamic(() => Promise.resolve(ClientOnly), { ssr: false });
--- code/backend-Doctorg-main/text.py
+++
@@ -1,6 +0,0 @@
-import nltk
-
-# Download necessary NLTK resources
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
